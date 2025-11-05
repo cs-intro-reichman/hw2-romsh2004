44e188f2e7190cb5e541c1b940615981b7178907
rom shevach <rom.shevach@post.runi.ac.il>
2025-11-05 09:27:28 +0200

    test

--- TimeCalc.java
+++ TimeCalc.java
@@ -8,13 +8,9 @@ public class TimeCalc {
         int minutestoadd = (minutesToAdd % 60);
         hours = hours + hourstoadd;
         minutes = minutes + minutestoadd;
-        if (minutes >= 60) {
-            hours = hours + (minutes / 60);
-            minutes = minutes % 60;
-        }
-        if (hours >= 24) {
-            hours = hours % 24;
-        }
+        hours = hours + (minutes / 60);
+        minutes = minutes % 60;
+        hours = hours % 24;
         System.out.println(hours + ":" + minutes);
 
 	}
