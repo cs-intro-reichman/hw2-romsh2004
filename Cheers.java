246eb9a84f5d299e44065b954d00fb157b392894
rom shevach <rom.shevach@post.runi.ac.il>
2025-11-05 14:03:06 +0200

    test

--- Cheers.java
+++ Cheers.java
@@ -2,19 +2,18 @@
 public class Cheers {
         public static void main(String[] args)
         {
-	String userInput = args[0];
+	String userInput = args[0].toUpperCase();
         int length = userInput.length();
         for (int i = 0 ; i < length; i++)
         {
                 String currentChar = String.valueOf(userInput.charAt(i));
-                currentChar = currentChar.toUpperCase();
                 if (currentChar.equals("A") || currentChar.equals("E") || currentChar.equals("F") || currentChar.equals("H") || currentChar.equals("I") || currentChar.equals("L") || currentChar.equals("M") || currentChar.equals("N") || currentChar.equals("O") || currentChar.equals("R") || currentChar.equals("S") || currentChar.equals("X"))
                 {
                         System.out.println("Give me an " + currentChar + ": " + currentChar + "!");
                 }
                 else
                 {
-                        System.out.println("Give me a " + currentChar + ": " + currentChar + "!");
+                        System.out.println("Give me a  " + currentChar + ": " + currentChar + "!");
                 }
         }
         System.out.println("What does that spell?");
