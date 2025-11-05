67efd0e305a7d2305cecd28a708e7eca07ec309d
rom shevach <rom.shevach@post.runi.ac.il>
2025-11-05 09:24:16 +0200

    test

--- TimeCalc.java
+++ TimeCalc.java
@@ -5,7 +5,7 @@ public class TimeCalc {
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
         int minutesToAdd = Integer.parseInt(args[1]);
 		int hourstoadd = (minutesToAdd / 60);
-        minutestoadd = (minutesToAdd % 60);
+        int minutestoadd = (minutesToAdd % 60);
         hours = hours + hourstoadd;
         minutes = minutes + minutestoadd;
         if (minutes >= 60) {
