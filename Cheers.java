041bf8a446d0e4d7b011c1808910ba6c4d1dc601
rom shevach <rom.shevach@post.runi.ac.il>
2025-11-05 11:12:48 +0200

    test

--- Cheers.java
+++ Cheers.java
@@ -17,7 +17,7 @@ public class Cheers {
                 }
         }
         System.out.println("What does that spell?");
-        for (int i = 0 ; i < length; i++)
+        for (int j = 0; j < Integer.parseInt(args[1]); j++)
         {
                 System.out.println(userInput + "!!!");
         }
