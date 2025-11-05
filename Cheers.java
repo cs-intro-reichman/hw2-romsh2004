b558c68adc05dc2d0282b5f88d7e983d81face81
rom shevach <rom.shevach@post.runi.ac.il>
2025-11-05 13:56:28 +0200

    test

--- Cheers.java
+++ Cheers.java
@@ -7,7 +7,8 @@ public class Cheers {
         for (int i = 0 ; i < length; i++)
         {
                 String currentChar = String.valueOf(userInput.charAt(i));
-                if (currentChar.equals("A") || currentChar.equals("E") || currentChar.equals("F") || currentChar.equals("H") || currentChar.equals("I") || currentChar.equals("L") || currentChar.equals("M") || currentChar.equals("N") || currentChar.equals("O") || currentChar.equals("R") || currentChar.equals("S") || currentChar.equals("X") ||currentChar.equals("a") || currentChar.equals("e") || currentChar.equals("f") || currentChar.equals("h") || currentChar.equals("i") || currentChar.equals("l") || currentChar.equals("m") || currentChar.equals("n") || currentChar.equals("o") || currentChar.equals("r") || currentChar.equals("s") || currentChar.equals("x"))
+                currentChar = currentChar.toUpperCase();
+                if (currentChar.equals("A") || currentChar.equals("E") || currentChar.equals("F") || currentChar.equals("H") || currentChar.equals("I") || currentChar.equals("L") || currentChar.equals("M") || currentChar.equals("N") || currentChar.equals("O") || currentChar.equals("R") || currentChar.equals("S") || currentChar.equals("X"))
                 {
                         System.out.println("Give me an " + currentChar + ": " + currentChar + "!");
                 }
